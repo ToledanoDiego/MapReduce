6a20a09554371ede09f3850732e8275a99bcf1b4
Diego Toledano <45643572+ToledanoDiego@users.noreply.github.com>
2021-10-04 18:55:44 +0200

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,47 @@
+from mrjob.job import MRJob
+from mrjob.step import MRStep
+import re
+WORD_RE = re.compile(r"[\w']+")
+
+#-- list of global values I use either to store some lists so it is easier to compute with them either some counters so things can happens only once
+values = []
+keys = []
+counter = 0
+
+class task(MRJob):
+
+#-- basic mapper that get the age value from the inputed file
+	def mapper(self, _, line):
+		data=line.split(", ")
+		if len(data)>=2:
+			age=data[0].strip()
+			yield (age, 1)
+		
+#-- basic reducer that count ages 
+	def reducer1(self, key, list_of_values):
+		global values
+		global keys
+		sumA = sum(list_of_values)
+		yield key, sumA
+		keys.append(key)
+		values.append(sumA)
+
+#-- reducer that output the maximum number of age and remove it from the map
+	def reducer2(self, key, list_of_values):
+		global counter
+		global values
+		global keys		
+		if counter < 1:
+			while len(values):
+				yield max(values), keys[values.index(max(values))]
+				keys.pop(values.index(max(values)))
+				values.remove(max(values))
+		counter = counter + 1
+
+#-- basic step
+	def steps(self):
+		return [MRStep(mapper=self.mapper, reducer=self.reducer1),MRStep(reducer=self.reducer2)]
+
+#-- basic start
+if __name__ == '__main__':
+	task.run()
---
+++ task3.py
@@ -0,0 +1,51 @@
+from mrjob.job import MRJob
+from mrjob.step import MRStep
+import re
+WORD_RE = re.compile(r"[\w']+")
+
+#-- list of global values I use either to store some lists so it is easier to compute with them either some counters so things can happens only once
+counter = 0
+counter2 = 0
+datas = []
+result = []
+
+class task(MRJob):
+
+#-- mapper that get the id value, the all the lines whiout the ids, and initialize the output from the inputed file
+	def mapper(self, _, line):
+		
+		global counter
+		global datas
+		data=line.split()
+		id=data[0].strip()
+		data.pop(0)
+		datas.append(data)
+		result.append([])
+		if counter < 10:
+			yield 1, 1
+		counter = counter + 1
+		
+#-- reducer that get the setup and return the output, here is an example of how it works: if 6 is in row 8, then output[6].append(8)		
+	def reducer1(self,key, list_of_values):
+		global counter2
+		global datas
+		global result
+		counter3 = 1
+		if counter2 < 1:
+			for id in datas:
+				for data in id:		
+
+					result[int(data)-1].append(counter3)
+					
+				counter3 = counter3 + 1
+			for x in result:
+				yield str(result.index(x)+1), [i for n, i in enumerate(x) if i not in x[:n]]
+		counter2 = counter2 + 1
+
+#-- basic step
+	def steps(self):
+		return [MRStep(mapper=self.mapper, reducer=self.reducer1)]
+
+#-- basic start
+if __name__ == '__main__':
+	task.run()
